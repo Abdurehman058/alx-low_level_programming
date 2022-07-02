753e170750d6dad52ba12015bb010a5eecbf1f43
Abdurehman058 <Abdurehmanmehdi@gmail.com>
2022-07-02 13:03:37 -0700

    task0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,12 @@
 #include <stdlib.h>
 #include <time.h>                                      
 #include <stdio.h>
-/**                                                     
-*main -> assign a randon number to the variable n each time it is executed and print                            *based a condition
-*Return : always 0
-*/                                                     
+/**
+ * main - assign a randon number to the variable n each time it is executed and print 
+ * based a condition
+ *
+ * Return: Always 0.
+ */                                                    
 int main(void)
 {
 int n;
@@ -15,8 +17,7 @@ if (n > 0)
 printf("%d is positive\n", n);
 if (n == 0)
 printf("%d is zero\n", n);
-if (n < 0)                                               
-{
+if (n < 0)                                              {
 printf("%d is negative\n", n);                      
 }                                                       
 return (0);
